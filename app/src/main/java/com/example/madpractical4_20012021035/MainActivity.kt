cd657dbb4cf041d84308f1ddbaa9039d1a6a4fe5
DELL <mitkumarpatel20@gnu.ac.in>
2022-09-14 20:13:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical4_20012021035/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.madpractical4_20012021035
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var buttonBrowse = findViewById<Button>(R.id.button_browse)
+        buttonBrowse.setOnClickListener {
+            var intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("https://www.google.com")).apply { startActivity(this) }
+        }
+
+        var buttoncall=findViewById<Button>(R.id.button_call)
+        buttoncall.setOnClickListener {
+            var intent=Intent(Intent.ACTION_DIAL)
+            intent.setData(Uri.parse("tel:+919726474961")).apply { startActivity(this) }
+        }
+    }
+}
